45a5fa59c58c4ffcde4a9a6190671b9f2734b42a
“Nishanthini-E” <nishanthini.elavazhagan@gmail.com>
2025-03-31 19:48:21 +0530

    commited code x with yy

--- src/main/java/basic/devopsDemo1.java
+++ src/main/java/basic/devopsDemo1.java
@@ -6,11 +6,10 @@ public class devopsDemo1 {
 		System.out.println("HELLO WORLD PROGRAM");
 
 	}
-	//from branch b1
+	//from branch b1. demo for stash code x added along with yy to fix the conflic
 
-public void m1() {//create new branch yy in git hub for stash demo 
+public void m1() {//create new branch yy in git hub for stash demo + //create new branch x in local for stash demo
 
-	
 	System.out.println("welcome to devops training - stash demo");
 }
 }
